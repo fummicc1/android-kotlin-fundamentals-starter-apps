f7e8ea947ce7a3849aa030caae70fda3f84e6486
fummicc1 <fumiya.tennis1@gmail.com>
2020-09-26 15:00:48 +0900

    wip

--- DiceRoller/app/src/main/java/dev/fummicc1/sample/dice_roller/MainActivity.kt
+++ DiceRoller/app/src/main/java/dev/fummicc1/sample/dice_roller/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.fummicc1.sample.dice_roller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,37 +12,31 @@ class MainActivity : AppCompatActivity() {
 
     var number: Int = 0
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
 
         rollButton.setOnClickListener {
             rollDice()
-        }
-
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener {
-            countUp()
         }
     }
 
     private fun rollDice() {
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-
-        val resultTextView: TextView = findViewById(R.id.result_text)
         val random = (1..6).random()
-        resultTextView.text = random.toString()
-
-        number = random
-    }
-
-    private  fun countUp() {
-        if (number == 6) {
-            return
+        val drawableResource = when (random) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        number += 1
-        val resultTextView: TextView = findViewById(R.id.result_text)
-        resultTextView.text = number.toString()
+        diceImage.setImageResource(drawableResource)
+        number = random
     }
 }
