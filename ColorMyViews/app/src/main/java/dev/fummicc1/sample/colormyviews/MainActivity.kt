d30b185743b65169ee0545d2bf14bfb16f7c5d7b
fummicc1 <fumiya.tennis1@gmail.com>
2020-09-26 23:54:13 +0900

    Android Kotlin Fundamentals: 02.3 ConstraintLayout using the Layout Edition

---
+++ ColorMyViews/app/src/main/java/dev/fummicc1/sample/colormyviews/MainActivity.kt
@@ -0,0 +1,52 @@
+package dev.fummicc1.sample.colormyviews
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> =
+            listOf(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton,
+            )
+
+        for (view in clickableViews) {
+            view.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.red_button -> view.setBackgroundResource(R.color.my_red)
+            R.id.green_button -> view.setBackgroundResource(R.color.my_green)
+            R.id.yellow_button -> view.setBackgroundResource(R.color.my_yellow)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
